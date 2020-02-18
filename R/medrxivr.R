a30b4fe743b832e0858c32caf7665ea85af3d40b
Luke McGuinness <luke.mcguinness@bristol.ac.uk>
2020-02-18 22:08:07 +0000

    small updates

---
+++ R/medrxivr.R
@@ -0,0 +1,7 @@
+#' medrxivr: Accessing medrXiv data from R
+#'
+#' The medrxivr package enables users to access data from the medrXiv
+#' preprint repository from R
+#' @docType package
+#' @name medrxivr
+NULL
