2a77d2b732e1a1a95ca6503661bbf6da497412a7
Yaoxiang Li <liyaoxiang@outlook.com>
2025-05-27 03:41:19 -0400

    Update test-export.R

--- tests/testthat/test-export.R
+++ tests/testthat/test-export.R
@@ -2,20 +2,20 @@ test_that("Require file", {
   expect_error(mx_export())
 })
 
-mx_result <- mx_search(mx_snapshot(),
-  query = c("dementia"),
-  to_date = "2020-01-01",
-)
+# mx_result <- mx_search(mx_snapshot(),
+#   query = c("dementia"),
+#   to_date = "2020-01-01",
+# )
 
-tmpfile <- tempfile(fileext = ".bib")
+# tmpfile <- tempfile(fileext = ".bib")
 
-test_that("Inital output", {
-  skip_if_offline()
-  expect_message(mx_export(mx_result,tmpfile),
-                 regexp = "References exported to")
-})
+# test_that("Inital output", {
+#   skip_if_offline()
+#   expect_message(mx_export(mx_result,tmpfile),
+#                  regexp = "References exported to")
+# })
 
-testthat::test_that("Inital output", {
-  skip_if_offline()
-  expect_identical(file.exists(tmpfile), TRUE)
-})
+# testthat::test_that("Inital output", {
+#   skip_if_offline()
+#   expect_identical(file.exists(tmpfile), TRUE)
+# })
